0541913e008ae549fba84bd6c2219d5fe3568092
nazdemirsoy <nazdemirsoyy@gmail.com>
2024-03-18 14:35:06 +0100

    Multiple Languages fix in Projects

--- src/app/components/projects.js
+++ src/app/components/projects.js
@@ -3,13 +3,13 @@ import styles from '../components/projects.module.css';
 const projects = [
     {
       name: "PixelRatings",
-      language: "React Native",
+      languages: ["React Native", "Firebase"],
       description: "A Game Rating Mobile Application.",
       link: "PixelRatings Documentation.pdf"
     }, 
     {
         name: "Tatooine",
-        language: "JavaScript",
+        languages: ["JavaScript"],
         description: "Spearheaded the design and development of a 2D real-time city builder game along the lines of the classic SimCity, as part of a collaborative team effort.",
         link: "Tatooine - User Documentation.pdf"
       },
@@ -24,7 +24,11 @@ export default function Projects() {
           <div key={index} className={styles.projectCard}>
             <div className={styles.projectHeader}>
               <h3 className={styles.projectTitle}>{project.name}</h3>
-              <span className={styles.projectLanguage}>{project.language}</span>
+              <div className={styles.projectLanguages}>
+                    {Array.isArray(project.languages) ? project.languages.map((language, langIndex) => (
+                  <span key={langIndex} className={styles.projectLanguage}>{language}</span>
+                )) : <span className={styles.projectLanguage}>{project.languages}</span>}
+              </div>
             </div>
             <p className={styles.projectDescription}>{project.description}</p>
             <a href={project.link} target="_blank" rel="noopener noreferrer" className={styles.projectLink}>View Project</a>
