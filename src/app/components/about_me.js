1b4b42238b28fb11f3573fb862a67a90c08f6a42
Naz Demirsoy <75198796+nazdemirsoyy@users.noreply.github.com>
2024-10-04 16:47:35 +0200

    Update about_me.js

--- src/app/components/about_me.js
+++ src/app/components/about_me.js
@@ -8,7 +8,7 @@ export default function AboutMe() {
         <h2 className={styles.heading}>About Me</h2>
         <div className={styles.cardContainer}>
             <p className="mb-4">
-                I am currently a masters student at <span className={styles.highlightText}>Georg-August-Universität</span> in Göttingen , with a solid academic background from <span className={styles.highlightText}>Eotvos Lorand University</span> where I specialized in web development, data science, and AI. My journey includes numerous certifications and a dedication to practical applications, as evidenced by my work on enhancing server efficiency through automation tools. You can download my CV from <a href="/Naz CV(1).pdf" download className={styles.link}>here</a>.              
+                I am currently a masters student at <span className={styles.highlightText}>Georg-August-Universität</span> in Göttingen , with a solid academic background from <span className={styles.highlightText}>Eotvos Lorand University</span> where I specialized in web development, data science, and AI. My journey includes numerous certifications and a dedication to practical applications, as evidenced by my work on enhancing server efficiency through automation tools. You can download my CV from <a href="/Naz CV.pdf" download className={styles.link}>here</a>.              
             </p>
             <p className="font-bold mb-2">Here are some technologies I have been working with:</p>
             <ul className={styles.technologyList}>
